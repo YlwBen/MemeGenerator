7ce278f451f87952701b624efd62b721b7571f85
Red <alexis.aformac@gmail.com>
2018-03-01 10:58:51 +0100

    push 2

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>MemeGenerator</title>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+        <link rel="stylesheet" type="text/css" href="style.css">
+    </head>
+    <body>
+      <section>
+        <div class="container">
+         <div class="row">
+           <img class="img col-6" src="img/meme.jpg" alt="">
+          <div class="input col-6">
+           <input type="text" method="get" name="" value="">
+           <input type="text" method="get" name="" value="">
+          </div>
+         </div>
+        </div>
+     </section>
+
+     <section>
+         <div class="container">
+          <div class="row">
+             <div class="col-3">
+                 <img class="icone" src="img/meme_un.jpg" alt="">
+             </div>
+             <div class="col-3">
+                 <img class="icone" src="img/meme_deux.jpg" alt="">
+             </div>
+             <div class="col-3">
+                 <img class="icone" src="img/meme_trois.jpg" alt="">
+             </div>
+             <div class="col-3">
+                 <img class="icone" src="img/meme_quatre.jpg" alt="">
+             </div>
+          </div>
+         </div>
+     </section>
+
+    </body>
+</html>
