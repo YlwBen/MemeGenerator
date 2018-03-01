a553043f6d46c268669cd6367ec505290e734914
Yellow <yellowaformac@gmail.com>
2018-03-01 11:20:49 +0100

    pull ben

---
+++ MemeGenerator/image.php
@@ -0,0 +1,15 @@
+<?php
+
+  header('Content-type: image/jpeg');
+
+  $image = imagecreatefromjpeg('img/meme.jpg');
+  $white = imagecolorallocate($image, 255, 255, 255);
+
+  $font_path = 'font/arial.ttf';
+  $text = $_GET['text']; // POUR LE FORM D'ALEXIS
+  imagettftext($image, 25, 0, 75, 300, $white, $font_path, $text);
+  imagejpeg($image);
+
+?>
+
+<?php <img src="image.php?text=montexte" /> ?>
