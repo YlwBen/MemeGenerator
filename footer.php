ef3222d79036a06bb67c80b3a83b049c4cf83669
Yellow <yellowaformac@gmail.com>
2018-03-06 09:26:35 +0100

    push includes

---
+++ footer.php
@@ -0,0 +1,6 @@
+
+
+    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
+    <script type="text/javascript" src="script.js"></script>
+    </body>
+</html>
