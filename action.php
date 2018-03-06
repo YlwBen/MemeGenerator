8659ca5b7a96847c92f872e042075fa41e7764e4
MikeMoleka <mike.moleka@gmail.com>
2018-03-06 09:45:29 +0100

    mise a jour

--- action.php
+++ action.php
@@ -1,16 +1,16 @@
 <?php
     include('controleur.php');
 
-    error_log("coucou je passe ici\n", 3, "log/test.log"); // Permet de voir les erreur dans le terminal (pour du php)
+    //error_log("coucou je passe ici\n", 3, "log/test.log"); // Permet de voir les erreur dans le terminal (pour du php)
     // var_dump($_POST);
     $url_complet  = $_POST['image'];
-    error_log($_POST['image'], 3, "log/test.log");
+    //error_log($_POST['image'], 3, "log/test.log");
 
     $url = strstr($url_complet, "img"); // Depuis PHP 5.3.0
     var_dump($url);
 
      //Set the Content Type
-     // header('Content-type: image/jpeg');
+
 
      // Create Image From Existing File
      $jpg_image = imagecreatefromjpeg($url);
@@ -22,7 +22,9 @@
      // // Set Path to Font File
      // $font_path = "Arial.ttf";
      $font = "/usr/share/fonts/truetype/freefont/FreeSerif.ttf";
+     var_dump($font);
 
+     $indice_fonts = imageloadfont('./04b.gdf');
 
      // // Set Text to Be Printed On Image
      $textTop = $_POST['textTop'];
@@ -34,8 +36,8 @@
 
      // // Print Text On Image
      $white = imagecolorallocate($jpg_image, 255, 255, 255);
-     imagettftext($jpg_image, 25, 0, 75, 300, $white, $font, $textTop);
-
+     imagettftext($jpg_image, 25, 0, 75, 300, $white, $fonts, $textTop);
+     //header('Content-type: image/png');
      echo '<img class="img" src="'.$url.'">';
 
      // // Send Image to Browser
