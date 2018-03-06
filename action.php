09b71469b0e3a55412abcb282d16b1cc5b6e19df
MikeMoleka <mike.moleka@gmail.com>
2018-03-06 11:45:42 +0100

    mise à jour

--- action.php
+++ action.php
@@ -1,48 +1,24 @@
 <?php
     include('controleur.php');
+    include('header.php');
 
-    //error_log("coucou je passe ici\n", 3, "log/test.log"); // Permet de voir les erreur dans le terminal (pour du php)
-    // var_dump($_POST);
     $url_complet  = $_POST['image'];
-    //error_log($_POST['image'], 3, "log/test.log");
-
     $url = strstr($url_complet, "img"); // Depuis PHP 5.3.0
-    var_dump($url);
+    $image = new Imagick($url);
+    $image->setResolution( 300, 300 );
+    $image->writeImage('/var/www/MemeGenerator/mypdf6666.jpeg');
+    //$im = new Imagick($url);
+    //header('Content-type: image/jpeg');
 
-     //Set the Content Type
+    //$image = new Imagick($url);
 
+// If 0 is provided as a width or height parameter,
+// aspect ratio is maintained
 
-     // Create Image From Existing File
-     $jpg_image = imagecreatefromjpeg($url);
-
-     // // Allocate A Color For The Text
-     // $white = imagecolorallocate($jpg_image, 255, 255, 255);
-
-
-     // // Set Path to Font File
-     // $font_path = "Arial.ttf";
-     $font = "/usr/share/fonts/truetype/freefont/FreeSerif.ttf";
-     var_dump($font);
-
-     // // Set Text to Be Printed On Image
-     $textTop = $_POST['textTop'];
-     var_dump($textTop);
-
-     $textBottom = $_POST['textBottom'];
-     var_dump($textBottom);
+    var_dump($image);
+    echo '<img src="'.$image.'" />';
 
 
-     // // Print Text On Image
-     $white = imagecolorallocate($jpg_image, 255, 255, 255);
-     imagettftext($jpg_image, 25, 0, 75, 300, $white, $font, $textTop);
-     //header('Content-type: image/png');
-     echo '<img class="img" src="'.$url.'">';
-
-     // // Send Image to Browser
-     // //imagejpeg($jpg_image);
-     //
-     // // Clear Memory
-     // imagedestroy($jpg_image);
-
+    include('footer.php');
 
 ?>
